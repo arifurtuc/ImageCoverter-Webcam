b18331c58c11b59869b3161b21453fc9a222fc0b
arif <arifur.tuc@gmail.com>
2023-11-04 10:00:42 +0100

    Add Streamlit camera input and image processing

--- converter.py
+++ converter.py
@@ -1,8 +1,16 @@
 import streamlit as st
 from PIL import Image
 
-camera_image = st.camera_input("Camera")
+# Create an expander to start the camera
+with st.expander("Start Camera"):
+    # Capture an image from the camera input
+    camera_image = st.camera_input("Camera")
 
-img = Image.open(camera_image)
-gray_img = img.convert("L")
-st.image(gray_img)
+# Check if an image was captured
+if camera_image:
+    # Open and process the captured image
+    img = Image.open(camera_image)
+    gray_img = img.convert("L")
+
+    # Display the processed image
+    st.image(gray_img)
