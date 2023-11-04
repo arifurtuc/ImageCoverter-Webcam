026208fbfa38835e902adc4dbb75f922591f078f
arif <arifur.tuc@gmail.com>
2023-11-04 10:13:31 +0100

    Add image processing for user uploads

--- converter.py
+++ converter.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from PIL import Image
 
+st.subheader("Color to Grayscale Converter")
+
+# Create a file uploader component allowing the user to upload a file
+uploaded_image = st.file_uploader("Upload Image")
+
 # Create an expander to start the camera
 with st.expander("Start Camera"):
     # Capture an image from the camera input
@@ -14,3 +19,12 @@ if camera_image:
 
     # Display the processed image
     st.image(gray_img)
+
+# Check if the image exists meaning the user has uploaded a file
+if uploaded_image:
+    # Open the user uploaded image with PIL
+    img = Image.open(uploaded_image)
+    # Convert the image to grayscale
+    gray_uploaded_img = img.convert('L')
+    # Display the grayscale image on the webpage
+    st.image(gray_uploaded_img)
